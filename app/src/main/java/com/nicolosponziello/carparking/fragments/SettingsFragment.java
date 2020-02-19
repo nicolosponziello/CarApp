790d1b9588f7bcbf289f88b4f642c53d9207a4c0
nicolosponziello <nicolosponziello@gmail.com>
2020-02-19 08:50:36 +0100

    Update preference activity

--- app/src/main/java/com/nicolosponziello/carparking/fragments/SettingsFragment.java
+++ app/src/main/java/com/nicolosponziello/carparking/fragments/SettingsFragment.java
@@ -6,23 +6,41 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 
 import com.nicolosponziello.carparking.MainActivity;
 import com.nicolosponziello.carparking.R;
 import com.nicolosponziello.carparking.database.DatabaseHelper;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    private Preference showIntro;
+    private Preference deleteDb;
+    private SwitchPreference enableNotif;
+    private EditTextPreference notifBeforeTime;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
-        findPreference(getString(R.string.show_intro_key)).setOnPreferenceClickListener((preference) ->{
+        showIntro = findPreference(getString(R.string.show_intro_key));
+        notifBeforeTime = findPreference(getString(R.string.notif_key));
+        deleteDb = findPreference(getString(R.string.delete_db_key));
+        enableNotif = findPreference(getString(R.string.enable_notif_key));
+
+        showIntro.setOnPreferenceClickListener((preference) ->{
             SharedPreferences preferences = getActivity().getSharedPreferences(getString(R.string.shared_prefs), Context.MODE_PRIVATE);
             preferences.edit().putBoolean(getString(R.string.intro_shown), false).commit();
             return true;
         });
-        findPreference(getString(R.string.delete_db_key)).setOnPreferenceClickListener(preference -> {
+
+
+        deleteDb.setOnPreferenceClickListener(preference -> {
             getActivity().getApplicationContext().deleteDatabase(DatabaseHelper.DATABASE_NAME);
 
             //restart app after deleteting database
@@ -36,5 +54,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             System.exit(0);
             return true;
         });
+
+
+        notifBeforeTime.setEnabled(enableNotif.isChecked());
+
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this::OnSharedPreferenceChangeListener);
+
+    }
+
+    //listen for changes
+    public void OnSharedPreferenceChangeListener(SharedPreferences shared, String key) {
+        if(key.equals(getString(R.string.enable_notif_key))){
+            notifBeforeTime.setEnabled(enableNotif.isChecked());
+        }
     }
 }
