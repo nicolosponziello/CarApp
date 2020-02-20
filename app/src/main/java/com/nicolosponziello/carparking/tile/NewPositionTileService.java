5b44127d201148a8a996f1ff973171d6df1c0f44
nicolosponziello <nicolosponziello@gmail.com>
2020-02-20 13:25:12 +0100

    Add tile to save new position

---
+++ app/src/main/java/com/nicolosponziello/carparking/tile/NewPositionTileService.java
@@ -0,0 +1,45 @@
+package com.nicolosponziello.carparking.tile;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.nicolosponziello.carparking.activity.NewParkActivity;
+import com.nicolosponziello.carparking.model.ParkManager;
+
+public class NewPositionTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        Tile tile = getQsTile();
+
+        if(ParkManager.getInstance(getApplicationContext()).hasActiveParking() || !hasLocationPermissions()){
+            tile.setState(Tile.STATE_UNAVAILABLE);
+        }else{
+            tile.setState(Tile.STATE_INACTIVE);
+        }
+        tile.updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        Intent intent = new Intent(getApplicationContext(), NewParkActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplication().startActivity(intent);
+
+    }
+
+
+    private boolean hasLocationPermissions(){
+        if(ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+}
