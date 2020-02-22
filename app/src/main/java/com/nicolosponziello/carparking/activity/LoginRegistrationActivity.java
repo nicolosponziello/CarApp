cfcb74fb980e12223aa2008a8293cc298cd04c3a
nicolosponziello <nicolosponziello@gmail.com>
2020-02-22 11:43:02 +0100

    add registration/login mode change

--- app/src/main/java/com/nicolosponziello/carparking/activity/LoginRegistrationActivity.java
+++ app/src/main/java/com/nicolosponziello/carparking/activity/LoginRegistrationActivity.java
@@ -9,12 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import com.nicolosponziello.carparking.R;
+import com.nicolosponziello.carparking.fragments.LoginFragment;
 import com.nicolosponziello.carparking.fragments.RegistrationFragment;
 
 public class LoginRegistrationActivity extends AppCompatActivity {
 
     private Button changeLoginReg;
     private FrameLayout fragmentHost;
+
+    private boolean registration;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,11 +27,20 @@ public class LoginRegistrationActivity extends AppCompatActivity {
         changeLoginReg = findViewById(R.id.changeLogReg);
         fragmentHost = findViewById(R.id.fragmentHost);
 
+        registration = true;
         FragmentManager manager = getSupportFragmentManager();
         manager.beginTransaction().add(R.id.fragmentHost, new RegistrationFragment()).commit();
 
         changeLoginReg.setOnClickListener(v -> {
-
+            if(registration){
+                //show login
+                registration = false;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentHost, new LoginFragment()).commit();
+            }else{
+                //show registration
+                registration = true;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentHost, new RegistrationFragment()).commit();
+            }
         });
 
     }
