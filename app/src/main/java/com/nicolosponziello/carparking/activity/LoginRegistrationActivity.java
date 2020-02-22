7531ea3557dda7fac5d1ed3171562f8354a03ba0
nicolosponziello <nicolosponziello@gmail.com>
2020-02-22 14:05:58 +0100

    Add login, register and recover functionality

--- app/src/main/java/com/nicolosponziello/carparking/activity/LoginRegistrationActivity.java
+++ app/src/main/java/com/nicolosponziello/carparking/activity/LoginRegistrationActivity.java
@@ -3,18 +3,18 @@ package com.nicolosponziello.carparking.activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.FrameLayout;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-
 import com.nicolosponziello.carparking.R;
 import com.nicolosponziello.carparking.fragments.LoginFragment;
+import com.nicolosponziello.carparking.fragments.RecoverPasswordFragment;
 import com.nicolosponziello.carparking.fragments.RegistrationFragment;
+import com.nicolosponziello.carparking.util.Const;
 
 public class LoginRegistrationActivity extends AppCompatActivity {
 
-    private Button changeLoginReg;
+    private Button changeLoginReg, recoverBtn;
     private FrameLayout fragmentHost;
 
     private boolean registration;
@@ -26,23 +26,53 @@ public class LoginRegistrationActivity extends AppCompatActivity {
 
         changeLoginReg = findViewById(R.id.changeLogReg);
         fragmentHost = findViewById(R.id.fragmentHost);
+        recoverBtn = findViewById(R.id.recoverBtn);
 
         registration = true;
         FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().add(R.id.fragmentHost, new RegistrationFragment()).commit();
+        Mode mode = (Mode) getIntent().getSerializableExtra(Const.LOGIN_REG_REC_MODE);
+        if(mode != null) {
+            switch (mode) {
+                case LOGIN:
+                    manager.beginTransaction().add(R.id.fragmentHost, new LoginFragment()).commit();
+                    break;
+                case RECOVER:
+                    manager.beginTransaction().add(R.id.fragmentHost, new RecoverPasswordFragment()).commit();
+                    break;
+                case REGISTER:
+                    manager.beginTransaction().add(R.id.fragmentHost, new RegistrationFragment()).commit();
+                    break;
+                default:
+                    manager.beginTransaction().add(R.id.fragmentHost, new RegistrationFragment()).commit();
+            }
+        }else{
+            manager.beginTransaction().add(R.id.fragmentHost, new RegistrationFragment()).commit();
+        }
 
         changeLoginReg.setOnClickListener(v -> {
             if(registration){
                 //show login
                 registration = false;
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmentHost, new LoginFragment()).commit();
+                changeLoginReg.setText(R.string.login_to_reg);
             }else{
                 //show registration
                 registration = true;
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragmentHost, new RegistrationFragment()).commit();
+                changeLoginReg.setText(R.string.reg_to_login);
             }
         });
 
+        recoverBtn.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentHost, new RecoverPasswordFragment()).commit();
+        });
+
+    }
+
+    public static enum Mode {
+        LOGIN,
+        REGISTER,
+        RECOVER
     }
 
 
---
+++ app/src/main/java/com/nicolosponziello/carparking/fragments/RecoverPasswordFragment.java
@@ -0,0 +1,64 @@
+package com.nicolosponziello.carparking.fragments;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.nicolosponziello.carparking.R;
+
+public class RecoverPasswordFragment extends Fragment {
+
+    private EditText emailInput;
+    private Button recoverBtn;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.recover_fragment, container, false);
+
+        emailInput = view.findViewById(R.id.recoverEmailInput);
+        recoverBtn = view.findViewById(R.id.recoverActionBtn);
+
+        recoverBtn.setClickable(false);
+
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(emailInput.getText().length() > 0){
+                    recoverBtn.setClickable(true);
+                }else{
+                    recoverBtn.setClickable(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        emailInput.addTextChangedListener(watcher);
+
+        recoverBtn.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().sendPasswordResetEmail(emailInput.getText().toString()).addOnCompleteListener(task ->{
+                if(task.isSuccessful()){
+                    Toast.makeText(getActivity(), "Link inviato per email!", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getActivity(), "Errore, riprovare", Toast.LENGTH_LONG).show();
+                }
+            });
+        });
+        return view;
+    }
+}
