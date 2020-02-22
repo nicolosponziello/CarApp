d39170521ad70c48971cbc7806488ae0db5011f1
nicolosponziello <nicolosponziello@gmail.com>
2020-02-22 09:08:37 +0100

    add toolbar to about activity

--- app/src/main/java/com/nicolosponziello/carparking/activity/AboutActivity.java
+++ app/src/main/java/com/nicolosponziello/carparking/activity/AboutActivity.java
@@ -1,9 +1,12 @@
 package com.nicolosponziello.carparking.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.nicolosponziello.carparking.R;
 
@@ -12,5 +15,21 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = findViewById(R.id.toolbarAbout);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.about_activity_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
